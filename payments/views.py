f9a5aba5abbec226aab4102ea253a194ac0e6e26
makskravchuk <maksimkravcuk14@gmail.com>
2023-04-14 21:54:29 +0300

    lr5_redis

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,10 +16,17 @@ class PaymentListAPIView(ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
+    @method_decorator(vary_on_headers("Authorization", ))
+    @method_decorator(cache_page(60))
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
+
 
 class PaymentAPIView(APIView):
     permission_classes = [IsAuthenticatedPermission, IsAdminPermission]
 
+    @method_decorator(vary_on_headers("Authorization", ))
+    @method_decorator(cache_page(60))
     def get(self, request, pk):
         payment = Payment.objects.get(pk=pk)
         serializer = PaymentSerializer(payment)
